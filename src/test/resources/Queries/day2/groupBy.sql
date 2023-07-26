17e904a0516b468753498e06f4d52dfd93c28335
Tmaidans <tmaidans@yahoo.com>
2023-07-26 01:19:12 -0400

    Firs upload

--- src/test/resources/Queries/day2/groupBy.sql
+++ src/test/resources/Queries/day2/groupBy.sql
@@ -12,8 +12,45 @@ where JOB_ID = 'IT_PROG';
 select Avg (SALARY) from EMPLOYEES
 where JOB_ID = 'AC_ACCOUNT';
 
--- group ny job id, and show all requested specifications.
+-- group by job id, and show all requested specifications.
 select JOB_ID, avg (SALARY), count(*), sum(SALARY), min(SALARY)
 from EMPLOYEES
 group by JOB_ID;
+
+--display how many departments we have in each location
+select LOCATION_ID, count(*)
+from DEPARTMENTS
+group by LOCATION_ID;
+
+--order result based on number of departments in desc
+select LOCATION_ID, count(*)
+from DEPARTMENTS
+group by LOCATION_ID
+order by count(*) desc;
+
+--order by column name instead of name of the column.
+select LOCATION_ID, count(*)
+from DEPARTMENTS
+group by LOCATION_ID
+order by 2 desc;
+
+--display how many countries we have in each region
+select * from COUNTRIES;
+
+select REGION_ID, count(*) from COUNTRIES
+group by REGION_ID
+order by 2 desc ;
+
+--get me total salary for each department from employees table
+select * from EMPLOYEES;
+select DEPARTMENT_ID, sum(SALARY),count(*)
+from EMPLOYEES
+group by DEPARTMENT_ID
+order by 2;
+
+select DEPARTMENT_ID, sum(SALARY),count(*)
+from EMPLOYEES
+where DEPARTMENT_ID is not null
+group by DEPARTMENT_ID
+order by 2;
 
